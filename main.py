8ea9ef218131cdf365b7aca307c42d334d2a59e2
Cherniakof Vlad <chernyackoff.vlad@yandex.ru>
2018-12-08 20:59:21 +0300

    Switched from py to ui

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # author Cherniakof Vlad
 # safe for passwords
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5 import uic
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Authorisation.ui',self)
+        self.LogInBtn.clicked.connect(self.LogIn)
+
+    def LogIn(self):
+        try:
+            file = open('Account.txt', 'r')
+        except FileNotFoundError:
+            self.ErrorText.setText('Ошибка. Создайте аккаунт')
+        login = self.LoginFld.text()
+        password = self.passwrdFld.text()
+        if login + password != file.readline()
+
+app = QApplication(sys.argv)
+window = MyWidget()
+window.show()
+sys.exit(app.exec_())
